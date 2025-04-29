711f664c34edd8faeed3c19f0636e0e8d46ed5b8
trethore <titou.rethore@gmail.com>
2025-04-29 15:25:18 +0200

    first commit

---
+++ frontend/src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+reportWebVitals();
